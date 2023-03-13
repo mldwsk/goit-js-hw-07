b56afe46cdffa94531b163adb9bfe29a2ad34086
michaldaw <119760691+michaldawiskiba@users.noreply.github.com>
2023-03-13 23:01:43 +0100

    Trying to solve 1.2

--- js/task-01.js
+++ js/task-01.js
@@ -1,3 +1,26 @@
+//task 1.1: count all categories
 const categoriesArray = document.querySelectorAll("li.item");
 const numberOfCategories = categoriesArray.length;
-console.log(numberOfCategories);
+console.log("Number of categories: " + numberOfCategories);
+
+//task 1.2: display every category and number of items inside of it
+let categoryAndItemNames = [];
+for (let i = 0; i < numberOfCategories; i++) {
+  categoryAndItemNames.push(categoriesArray[i].textContent);
+  console.log(categoriesArray[i]);
+}
+
+let categoriesArrayChildren = [];
+for (let i = 0; i < numberOfCategories; i++) {
+    categoriesArrayChildren.push(categoriesArray[i].children);
+    console.log(categoriesArrayChildren[i]);
+  }
+/*
+for (let i = 0; i < numberOfCategories; i++) {
+  console.log(categoryNamesAndItemCount[i]);
+}
+
+/*
+for (let i = 0; i < numberOfCategories; i++) {
+    console.log(categoriesArray[i].children);
+}*/
