79cbdc226bdf32714fb8ac48a05b96f8b3352995
michaldaw <119760691+michaldawiskiba@users.noreply.github.com>
2023-03-14 17:47:49 +0100

    Solved task 2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,14 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+for (let i = 0; i < ingredients.length; i++) {
+  const ingredientList = document.createElement("li");
+  ingredientList.innerHTML = ingredients[i];
+  ingredientList.setAttribute("class", "item");
+  document.getElementById("ingredients").appendChild(ingredientList);
+}
