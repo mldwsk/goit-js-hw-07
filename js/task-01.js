fc97d96389c80721e2b883df8d378d1ad2521514
michaldaw <119760691+michaldawiskiba@users.noreply.github.com>
2023-03-14 15:40:43 +0100

    Optimized code for task 1.2

--- js/task-01.js
+++ js/task-01.js
@@ -7,17 +7,14 @@ console.log("Number of categories: " + numberOfCategories);
 //task 1.2: display every category and number of items inside of it
 let categoriesAndItemsTextArray = [];
 for (let i = 0; i < categoryItems.length; i++) {
-  if (categoryItems[i].nodeName === "LI") {
-    categoriesAndItemsTextArray[i] = categoryItems[i].innerText;
-  }
+  categoriesAndItemsTextArray[i] = categoryItems[i].innerText;
 }
 let arrayOfCategoryArrays = [];
-for(let i = 0; i < categoriesAndItemsTextArray.length; i++) 
-{
-  arrayOfCategoryArrays[i] = categoriesAndItemsTextArray[i].split("\n");   
+for (let i = 0; i < categoriesAndItemsTextArray.length; i++) {
+  arrayOfCategoryArrays[i] = categoriesAndItemsTextArray[i].split("\n");
 }
 
-for(let i = 0; i<arrayOfCategoryArrays.length; i++) {
-    console.log("Category: " + arrayOfCategoryArrays[i][0]);
-    console.log("Elements: " + (arrayOfCategoryArrays[i].length-1));
+for (let i = 0; i < arrayOfCategoryArrays.length; i++) {
+  console.log("Category: " + arrayOfCategoryArrays[i][0]);
+  console.log("Elements: " + (arrayOfCategoryArrays[i].length - 1));
 }
