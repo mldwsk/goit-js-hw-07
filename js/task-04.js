4ba1bdae7dc84d21709ca887719749b35f92e31f
michaldaw <119760691+michaldawiskiba@users.noreply.github.com>
2023-03-14 23:15:27 +0100

    Solved task 4, bug in createBoxes() NOT fixed yet

--- js/task-04.js
+++ js/task-04.js
@@ -1,33 +1,49 @@
-/*
-Write a script to create and clear a collection of elements.
-The user enters the number of elements into input and clicks the New button,
-after which a collection is rendered. 
-When you click on the Clear button, the collection is cleared.
-*/
-
-/*
-<div id="boxes"></div>
-
-Create a createBoxes(amount) function that takes one parameter, a number. The function creates as many <div> as specified in amount and adds them to div#boxes.
-
-The dimensions of the very first <div> are 30px by 30px.
-Each element after the first one should be 10px wider and higher than the previous one.
-All elements must have a random HEX background color. Use the ready-made getRandomHexColor function to get a color.
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
 
-Create a destroyBoxes() function that clears the contents of div#boxes, thereby removing all created elements.
+function createBoxes(amount) {
+  const allBoxes = document.createDocumentFragment();
+  for (let i = 0; i < amount; i++) {
+    let size = `${30 + i * 10}px`;
+    const singleBox = document.createElement("div");
+    singleBox.style.backgroundColor = getRandomHexColor();
+    singleBox.style.width = size;
+    singleBox.style.height = size;
+    console.log(singleBox);
+    allBoxes.appendChild(singleBox);
+  }
+  document.getElementById("boxes").appendChild(allBoxes);
+}
 
-let buttonHTML = '<div id="controls">
-  <input type="number" min="1" max="100" step="1" />
-  <button type="button" data-create>Create</button>
-  <button type="button" data-destroy>Destroy</button>
-</div>';
+function destroyBoxes() {
+  const listOfBoxes = document.getElementById("boxes");
+  while (listOfBoxes.hasChildNodes()) {
+    listOfBoxes.removeChild(listOfBoxes.firstChild);
+  }
+}
 
-const whereToPutHTML = document.getElementByID("counter");
-whereToPutHTML.insertAdjacentHTML("afterbegin", buttonHTML);
+const createButton = document.querySelector("[data-create]");
+const destroyButton = document.querySelector("[data-destroy]");
+const inputArea = document.querySelector("input");
+let specifiedAmount = 0;
 
-*/
+inputArea.addEventListener("input", (event) => {
+  event.preventDefault();
+  const chosenAmount = event.currentTarget;
+  if (chosenAmount.value >= 1 && chosenAmount.value <= 99) {
+    specifiedAmount = chosenAmount.value;
+  } else if (chosenAmount.value > 100) {
+    specifiedAmount = 100;
+  } else if (chosenAmount.value < 0) {
+    specifiedAmount = 1;
+  } else {
+    specifiedAmount = 0;
+  }
+  console.log(specifiedAmount);
+});
+
+createButton.addEventListener("click", createBoxes(specifiedAmount));
+destroyButton.addEventListener("click", destroyBoxes);
